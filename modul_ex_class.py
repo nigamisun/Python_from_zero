bef2e741d54b1fc39493688a4749760e6ab6b3cc
Azamat <totiazad@gmail.com>
2025-07-31 01:17:42 +0500

    Delete modul_ex_class.py

--- modul_ex_class.py
+++
@@ -1,9 +0,0 @@
-class My_Animal:
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-    
-    def con(self):
-        print(f"{self.name} is a {self.species}")
-
-    
