4dd23ac0a1bff3fe5a1d598c35685f5bbf4f2326
Azamat <totiazad@gmail.com>
2025-07-31 01:11:29 +0500

    chapter_4

---
+++ modul_ex_class.py
@@ -0,0 +1,9 @@
+class My_Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+    
+    def con(self):
+        print(f"{self.name} is a {self.species}")
+
+    
