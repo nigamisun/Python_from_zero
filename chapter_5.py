fe686f707d631af54fa1ba5dbd5cdb54b4f1645a
Azamat <totiazad@gmail.com>
2025-08-02 03:47:22 +0500

    Update chapter_5.py with data structures examples

--- chapter_5.py
+++ chapter_5.py
@@ -53,3 +53,77 @@ if "Anna" in ages:
     print(ages["Anna"])
 else:
     pass
+
+nums = [4, 5, 6, 2, 8, 1, 2, 3,]
+colors = ["green", "red", "blue", "white", "black"]
+copy = nums.copy()
+clone = nums[:]
+sliced_nums = nums[1:4]
+sliced_nums_re = nums[-6:-2]
+sliced_nums_st = nums[0:6:2]
+sliced_nums_st_re = nums[-6:-1:2]
+pairs = [(n, c) for n in nums for c in colors]
+pairs_p = list(zip(nums, colors))
+
+import itertools as it
+pairs_p_c = list(zip(nums, it.cycle(colors)))
+
+print(copy)
+print(clone)
+print(sliced_nums)
+print(sliced_nums_re)
+print(sliced_nums_st)
+print(sliced_nums_st_re)
+print(pairs)
+print(pairs_p)
+print(pairs_p_c)
+
+my_dict = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7}
+new_dict = {k: v * v for k, v in my_dict.items() if v > 1 and v < 7}
+print(new_dict)
+
+my_list = [('a', 1), ('b', 2), ('c', 3), ('d', 4)]
+my_dict_1 = {k: v for k, v in my_list}
+print(my_dict_1)
+
+new_dict_z = {n: c for n, c in zip(nums, colors)}
+print(new_dict_z)
+new_dict_d = dict(zip(nums, colors))
+print(new_dict_d)
+
+my_set = set(colors)
+num_set = set(nums)
+
+union_set = my_set | num_set        
+union_set1 = my_set.union(num_set)
+
+num = set(numbers)
+numl = set(numbersl)
+numl.add(9)
+inter = num & numl
+inter1 = num.intersection(numl)
+difer = num - numl
+difer_re = numl - num
+difer1 = num.difference(numl)
+sym_difer = num ^ numl
+sym_difer1 = num.symmetric_difference(numl)
+
+
+print(my_set)
+print(num_set)
+print(union_set)
+print(union_set1)
+print(num)
+print(numl)
+print(inter)
+print(inter1)
+print(difer)
+print(difer1)
+print(difer_re)
+print(sym_difer)
+print(sym_difer1)
+
+numl.discard(9)
+print(numl)
+num.remove(5)
+print(num)
